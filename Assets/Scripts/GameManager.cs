0fc2a55c37a085d427edc944a5ff2b48f6053166
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-05-03 18:26:11 -0400

    Added a smooth movement script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,24 @@ using Photon.Pun;
 public class GameManager : MonoBehaviourPunCallbacks
 {
 
-    [SerializeField] GameObject player;
+    [SerializeField] GameObject platformerPlayer;
+    [SerializeField] GameObject tetrisPlayer;
+
+
+    // Used to keep a singleton
 	public static GameManager Instance = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Singleton behavior
 		if(Instance == null) 
 		{
 			Instance = this;
 			Debug.Log("Instantiated new player");
-			PhotonNetwork.Instantiate(player.name, new Vector3(0f, 3f, 0f), Quaternion.identity);
-		} 
+            // TODO: Make the tetris player get instantiated as well
+            PhotonNetwork.Instantiate(platformerPlayer.name, new Vector3(0f, 3f, 0f), Quaternion.identity);
+        } 
 		else 
 		{
 			Destroy(gameObject);
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviourPun, IPunObservable
+{
+    public Vector3 realPosition = Vector3.zero;
+    public Vector3 positionAtLastPacket = Vector3.zero;
+    public double currentTime = 0.0;
+    public double currentPacketTime = 0.0;
+    public double lastPacketTime = 0.0;
+    public double timeToReachGoal = 0.0;
+
+    void Update()
+    {
+        if (!photonView.IsMine)
+        {
+            timeToReachGoal = currentPacketTime - lastPacketTime;
+            currentTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(positionAtLastPacket, realPosition, (float)(currentTime / timeToReachGoal));
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext((Vector3)transform.position);
+        }
+        else
+        {
+            currentTime = 0.0;
+            positionAtLastPacket = transform.position;
+            realPosition = (Vector3)stream.ReceiveNext();
+            lastPacketTime = currentPacketTime;
+            currentPacketTime = info.SentServerTime;
+        }
+    }
+}
